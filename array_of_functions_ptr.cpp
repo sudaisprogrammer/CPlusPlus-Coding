7ed21cec80c49195b521ad7b28b7c3ead7c008fa
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-23 22:30:01 -0700

    array of function pointers

--- array_of_functions_ptr.cpp
+++ array_of_functions_ptr.cpp
@@ -7,14 +7,14 @@ int add(int a,int b){
 int sub(int a,int b){
     return a-b;
 }
-int prod(int a,int b){
+int prod(int a,int b){ //main pointer funtions/driver function
     return a*b;
 }
 int main(){
     int(*operations[3])(int ,int);
-    operations[0] = add;
-    operations[1] = sub;
-    operations[2] = prod;
+    operations[0] = add; //assigining address of add function at operations[0]
+    operations[1] = sub; //assigining address of sub function at operations[1]
+    operations[2] = prod; //assigining address of prod function at operations[2]
     cout<<"Addition is "<<operations[0](2,3)<<endl;
     cout<<"difference is "<<operations[1](4,3)<<endl;
     cout<<"product is "<<operations[2](5,3)<<endl;
