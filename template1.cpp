9eb7da7ef561cfda98f8436a5399e38b9f277da0
sudaisprogrammer <sudais.programmer@gmail.com>
2026-08-09 22:47:17 +0500

    templates in c++

--- template1.cpp
+++ template1.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 using namespace std;
 
+template<typename T> //its like a function but work with any data type
+T add(T a , T b, T c){
+    return a+b+c;
+}
 int main(){
-
+    cout<<"sum is "<<add(1,2,3)<<endl;
+    cout<<"sum is "<<add(1,3,6)<<endl;
     return 0;
 }
